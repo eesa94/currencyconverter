2a537374f019b09bad5dd691de7594e010aeaad7
Eesa Patel <epatel@and.digital>
2019-06-03 12:14:09 +0100

    create home screen component with logo

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, ImageBackground, Image, Text } from 'react-native';
+import styles from './styles';
+
+const Logo = () => (
+  <View style={styles.container}>
+    <ImageBackground
+      style={styles.containerImage}
+      source={require('./images/background.png')}
+      resizeMode='contain'
+    >
+      <Image
+        style={styles.image}
+        source={require('./images/logo.png')}
+        resizeMode='contain'
+      />
+    </ImageBackground>
+    <Text style={styles.text}>Currency Converter</Text>
+  </View>
+);
+
+export default Logo;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
+
+EStyleSheet.build({
+  $primaryBlue: '#4f6d7a',
+  $white: '#fff'
+});
+
+const Index = () => <Home />;
+
+export default Index;
