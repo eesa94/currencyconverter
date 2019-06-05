7217f68b23a82efb59fc788dd50ff0ccc0ac0b59
Eesa Patel <epatel@and.digital>
2019-06-05 13:50:46 +0100

    home screen - animating the logo on keyboard show/hide

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,17 +4,27 @@ import { Dimensions } from 'react-native';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
   container: {
     alignItems: 'center'
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth
+    width: '$largeContainerSize',
+    height: '$largeContainerSize'
+  },
+  backgroundImage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    flex: 1
   },
   image: {
-    width: imageWidth / 2
+    width: '$largeImageSize'
   },
   text: {
     fontWeight: '600',
