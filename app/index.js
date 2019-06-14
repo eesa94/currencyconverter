e0999c57d73de16ab630077295e7fbe577ea70c2
Eesa Patel <epatel@and.digital>
2019-06-14 10:40:12 +0100

    currency list screen - flatlist and listitem

--- app/index.js
+++ app/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4f6d7a',
   $white: '#fff',
   $border: '#e2e2e2',
   $inputText: '#797979',
-  $lightGrey: '#f0f0f0'
+  $lightGrey: '#f0f0f0',
+  $darkText: '#343434'
   // $outline: 1
 });
 
 const Index = () => <Home />;
+const Currencies = () => <CurrencyList />;
 
-export default Index;
+export default Currencies;
